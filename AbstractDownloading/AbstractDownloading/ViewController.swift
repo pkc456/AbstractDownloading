90e0d1cdd80533d3d0b326af8109bd22aa927ebc
Pardeep <pkc456@gmail.com>
2016-07-31 23:38:02 +0530

    Added initial project files

---
+++ AbstractDownloading/AbstractDownloading/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AbstractDownloading
+//
+//  Created by Pardeep on 31/07/16.
+//  Copyright © 2016 www.programmingcrew.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
