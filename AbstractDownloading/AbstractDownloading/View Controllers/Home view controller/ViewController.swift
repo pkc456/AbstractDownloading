a81827643b76035daca996a455d39c929233912c
Pardeep <pkc456@gmail.com>
2016-08-01 23:36:46 +0530

    Added links to image urls

--- AbstractDownloading/AbstractDownloading/View Controllers/Home view controller/ViewController.swift
+++ AbstractDownloading/AbstractDownloading/View Controllers/Home view controller/ViewController.swift
@@ -28,7 +28,43 @@ class ViewController: UIViewController {
 
     func addUrlStringToArray(){
         arrayImages.addObject("https://raw.githubusercontent.com/wangjwchn/TestImage/master/Image004.jpg")
-        arrayImages.addObject("http://dummyimage.com/600x400/000/fff&text=ww")        
+        arrayImages.addObject("http://dummyimage.com/600x400/000/fff&text=ww")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage001.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage002.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage003.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage004.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage005.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage006.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage007.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage008.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage009.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage010.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage011.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage012.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage013.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage014.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage015.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage016.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage017.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage018.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage019.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage020.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage021.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage022.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage023.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage024.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage025.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage026.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage027.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage028.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage029.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage030.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage031.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage032.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage033.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage034.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage035.jpg")
+        arrayImages.addObject("https://s3.amazonaws.com/fast-image-cache/demo-images/FICDDemoImage036.jpg")
     }
     
     //MARK:- Table view delegate
@@ -44,6 +80,8 @@ class ViewController: UIViewController {
         let url = NSURL(string: arrayImages.objectAtIndex(indexPath.row) as! String)
         cell.imageUrl = url
         cell.customImageView.image = nil;
+        cell.buttonCross.hidden = false
+//        cell.customImageView.image = UIImage(named: "Placeholder")
         let photo = FICDPhoto()
         photo.sourceImageURL = url
         
